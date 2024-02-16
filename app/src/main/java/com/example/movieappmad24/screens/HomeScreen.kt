9040bed70038010a95c3f865661f2d98c8bc9f80
leonardo1710 <leon.freudenthaler@fh-campuswien.ac.at>
2024-02-16 12:46:37 +0100

    lecture stuff finished

--- app/src/main/java/com/example/movieappmad24/screens/HomeScreen.kt
+++ app/src/main/java/com/example/movieappmad24/screens/HomeScreen.kt
@@ -4,17 +4,26 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.movieappmad24.data.MovieDatabase
+import com.example.movieappmad24.data.MovieRepository
 import com.example.movieappmad24.viewmodels.MoviesViewModel
+import com.example.movieappmad24.viewmodels.MoviesViewModelFactory
 import com.example.movieappmad24.widgets.MovieList
 import com.example.movieappmad24.widgets.SimpleBottomAppBar
 import com.example.movieappmad24.widgets.SimpleTopAppBar
 
 @Composable
 fun HomeScreen(
-    navController: NavController,
-    moviesViewModel: MoviesViewModel
+    navController: NavController
 ) {
+    val db = MovieDatabase.getDatabase(LocalContext.current)
+    val repository = MovieRepository(movieDao = db.movieDao())
+    val factory = MoviesViewModelFactory(repository = repository)
+    val viewModel: MoviesViewModel = viewModel(factory = factory)
+
     Scaffold (
         topBar = {
             SimpleTopAppBar(title = "Movie App")
@@ -27,9 +36,9 @@ fun HomeScreen(
     ){ innerPadding ->
         MovieList(
             modifier = Modifier.padding(innerPadding),
-            movies = moviesViewModel.movies,
+            movies = viewModel.movies,
             navController = navController,
-            viewModel = moviesViewModel
+            viewModel = viewModel
         )
     }
 }
